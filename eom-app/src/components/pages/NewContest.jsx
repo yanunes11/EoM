f06eb6043bd6b16e6b41a3668b018ce21af08ff6
yannunes11 <yannunes112@gmail.com>
2022-11-14 15:53:00 -0300

    feat: New Contest css buttons

--- eom-app/src/components/pages/NewContest.jsx
+++ eom-app/src/components/pages/NewContest.jsx
@@ -1,29 +1,30 @@
 import React from "react";
-import Card from 'react-bootstrap/Card'; //YNASC
 import styles from '../css/NewContest.module.css';
 
 function NewContest() {
     return(
         <>
             <p className={styles.title}>New Contest</p>
-            <Card className={styles.container}>
+            <div className={styles.container}>
                 {/* FIELDS */}
-                Voting Starting Date: 
-                <p><input type="date"></input></p>
-                Voting Ending Date: 
-                <p><input type="date"></input></p>
-                Name: 
-                <p><input type="text"></input></p>
-                Description: 
-                <p><input type="text"></input></p>
-                State: 
-                <p><input type="date"></input></p>
+                <p>Voting Starting Date: </p>
+                <input type="date"></input>
+                <p>Voting Ending Date: </p>
+                <input type="date"></input>
+                <p>Name: </p>
+                <input type="text"></input>
+                <p>Description: </p>
+                <input type="text"></input>
+                <p>State: </p>
+                <input type="date"></input>
 
                 {/* BUTTONS */}
-                <button className={styles.btn}>Back</button>
-                <button className={styles.btn}>Save</button>
-                <button className={styles.btn}>Delete</button>
-            </Card>
+                <div>
+                    <button className={styles.cancel}>Back</button>
+                    <button className={styles.save}>Save</button>
+                    <button className={styles.delete}>Delete</button>
+                </div>
+            </div>
         </>
     )
 }
